c85c48e8dbf08acf326ef6bcb487883595a655f2
yocajii <yocajii@stone.sakura.ne.jp>
2022-06-09 15:52:39 +0900

    ToDoアプリを作成した

---
+++ main.js
@@ -0,0 +1,57 @@
+// localStorage persistence
+const STORAGE_KEY = 'my-todo'
+const todoStorage = {
+  fetch () {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach((todo, index) => {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+// app Vue instance
+const app = Vue.createApp({
+  data () {
+    return {
+      title: 'My ToDo',
+      todos: todoStorage.fetch(),
+      newTodo: ''
+    }
+  },
+
+  watch: {
+    todos: {
+      handler (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    addTodo () {
+      const value = this.newTodo && this.newTodo.trim()
+      if (!value) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      })
+      this.newTodo = ''
+    },
+
+    removeTodo (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+  }
+})
+
+// mount
+app.mount('.todoapp')
